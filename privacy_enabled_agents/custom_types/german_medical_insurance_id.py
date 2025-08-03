d05ae45c9a9d92809868a16abdf46b02c4146a13
Fabian Reinold <fabian.reinold@protonmail.com>
2025-08-03 15:30:30 +0200

    chore(extra_types): improve typing

--- privacy_enabled_agents/custom_types/german_medical_insurance_id.py
+++ privacy_enabled_agents/custom_types/german_medical_insurance_id.py
@@ -20,8 +20,8 @@ class GermanMedicalInsuranceID(str):
 
         """
         return core_schema.with_info_before_validator_function(
-            cls._validate,
-            core_schema.str_schema(),
+            function=cls._validate,
+            schema=core_schema.str_schema(),
         )
 
     @classmethod
@@ -51,31 +51,43 @@ class GermanMedicalInsuranceID(str):
         Raises:
             PydanticCustomError: If the input is not a valid id.
         """
-        input_length = len(value)
+        input_length: int = len(value)
         if input_length != 10:
-            raise PydanticCustomError("german_medical_insurance_id_length", f"ID must be 10 characters long, not {input_length}")
+            raise PydanticCustomError(
+                error_type="german_medical_insurance_id_length",
+                message_template="ID must be 10 characters long, not {value}",
+                context={"value": value},
+            )
 
-        first_letter = value[0].upper()
+        first_letter: str = value[0].upper()
         if not first_letter.isalpha():
             raise PydanticCustomError(
-                "german_medical_insurance_id_invalid_characters", f"First character of ID must be a letter, not {first_letter}"
+                error_type="german_medical_insurance_id_invalid_characters",
+                message_template="First character of ID must be a letter, not {value}",
+                context={"value": first_letter},
             )
 
-        other_letters = value[1:]
+        other_letters: str = value[1:]
         if not other_letters.isnumeric():
             raise PydanticCustomError(
-                "german_medical_insurance_id_invalid_characters", "All characters of ID after the first must be numbers."
+                error_type="german_medical_insurance_id_invalid_characters",
+                message_template="All characters of ID after the first must be numbers, not {value}",
+                context={"value": other_letters},
             )
 
-        first_letter_value = ord(first_letter) - ord("A") + 1
+        first_letter_value: int | str = ord(first_letter) - ord("A") + 1
         first_letter_value = f"{first_letter_value:02}"
 
-        digits = [int(digit) for digit in first_letter_value + other_letters]
-        checksum = digits.pop(-1)
+        digits: list[int] = [int(digit) for digit in first_letter_value + other_letters]
+        checksum: int = digits.pop(-1)
 
-        weights = [1, 2] * 5
-        weighted_digits_sum = sum([digit * weight for digit, weight in zip(digits, weights)])
+        weights: list[int] = [1, 2] * 5
+        weighted_digits_sum: int = sum([digit * weight for digit, weight in zip(digits, weights)])
 
-        calculated_checksum = weighted_digits_sum % 10
+        calculated_checksum: int = weighted_digits_sum % 10
         if calculated_checksum != checksum:
-            raise PydanticCustomError("german_medical_insurance_id_invalid_checksum", "Invalid ID checksum.")
+            raise PydanticCustomError(
+                error_type="german_medical_insurance_id_invalid_checksum",
+                message_template="Invalid ID checksum {calculated_checksum}, expected {checksum}",
+                context={"calculated_checksum": calculated_checksum, "checksum": checksum},
+            )
