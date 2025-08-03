bcaaa89d6f1f2c1a0ac0ec0ae0c935ca967a8d8d
Fabian Reinold <fabian.reinold@protonmail.com>
2025-08-03 17:47:24 +0200

    fix: enhance type hinting in MockEncryptionReplacer

--- privacy_enabled_agents/replacement/encryption.py
+++ privacy_enabled_agents/replacement/encryption.py
@@ -1,3 +1,4 @@
+from typing import Literal
 from uuid import UUID
 
 from privacy_enabled_agents.base import Entity
@@ -7,7 +8,7 @@ from privacy_enabled_agents.replacement.base import BaseReplacer
 class MockEncryptionReplacer(BaseReplacer):
     """A replacer only to be used with the EncryptionStorage class."""
 
-    _supported_entities = "ANY"  # Allow all entities
+    _supported_entities: set[str] | Literal["ANY"] = "ANY"  # Allow all entities  # noqa: F821
 
     def create_replacement(self, entity: Entity, context_id: UUID) -> str:
         raise NotImplementedError("MockEncryptionReplacer does not support create_replacement operation.")
