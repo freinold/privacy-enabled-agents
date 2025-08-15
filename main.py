6f9e61b2eb6ed3c70b8690997a61a16390c29997
Fabian Reinold <fabian.reinold@protonmail.com>
2025-08-15 20:43:42 +0200

    feat(cli): add option to run eval instead of frontend

--- main.py
+++ main.py
@@ -4,9 +4,40 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-from gradio.blocks import Blocks
+# regular imports from here on
+from pydantic import AliasChoices, Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-from privacy_enabled_agents.frontend.gradio import create_gradio_interface
 
-demo: Blocks = create_gradio_interface()
-demo.launch(server_name="localhost")
+class Settings(BaseSettings):
+    evaluation: bool = Field(
+        default=False,
+        validation_alias=AliasChoices("e", "eval"),
+        description="Run an evaluation instead of the frontend",
+    )
+
+    model_config = SettingsConfigDict(env_prefix="PEA_", cli_parse_args=True)
+
+
+def run_frontend() -> None:
+    from gradio.blocks import Blocks
+
+    from privacy_enabled_agents.frontend.gradio import create_gradio_interface
+
+    demo: Blocks = create_gradio_interface()
+    demo.launch(server_name="localhost")
+
+
+def main() -> None:
+    settings: Settings = Settings()
+
+    if settings.evaluation:
+        pass
+        # TODO: add eval options here
+
+    else:
+        run_frontend()
+
+
+if __name__ == "__main__":
+    main()
