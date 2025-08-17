bc3d7fbe6bd9efcd2e0d792dee0a692d3bc4d730
Fabian Reinold <fabian.reinold@protonmail.com>
2025-08-17 21:33:56 +0200

    chore: export new config correctly to submodule

--- privacy_enabled_agents/runtime/__init__.py
+++ privacy_enabled_agents/runtime/__init__.py
@@ -1,5 +1,8 @@
-from .agent import create_privacy_agent
+from .builder import create_privacy_agent
+from .config import PrivacyAgentConfig, PrivacyAgentConfigDict
 
 __all__: list[str] = [
     "create_privacy_agent",
+    "PrivacyAgentConfig",
+    "PrivacyAgentConfigDict",
 ]
