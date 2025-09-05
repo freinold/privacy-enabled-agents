25ab4d41824a46e5bfb25d10007a2eab4b4e139e
Fabian Reinold <fabian.reinold@protonmail.com>
2025-09-05 19:22:35 +0200

    chore: run app on 8080 port

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def run_frontend() -> None:
     from privacy_enabled_agents.frontend.gradio import create_gradio_interface
 
     demo: Blocks = create_gradio_interface()
-    demo.launch(server_name="localhost")
+    demo.launch(server_name="localhost", server_port=8080)
 
 
 def main() -> None:
